8eb6d885cd3341c5ae0b131b0b14b3c70d64c2aa
Phong <boushphong@github.com>
2022-05-07 05:09:31 +0700

    Sat  7 May 05:09:31 +07 2022 commit

--- docker/airflow/entrypoint.sh
+++ docker/airflow/entrypoint.sh
@@ -8,5 +8,8 @@ airflow scheduler &> /dev/null &
 # Create user
 airflow users create -u admin -p admin -r Admin -e admin@admin.com -f Phong -l Bui
 
+# Add Airbyte connection
+airflow connections add --conn-host localhost --conn-port 8000 --conn-type airbyte airbyte_conn
+
 # Run the web server in foreground (for docker logs)
 exec airflow webserver
